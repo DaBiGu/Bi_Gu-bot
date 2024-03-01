07c4e1e3e6e43693418a0373b3064c7c5dbd8191
Bi_Gu <bigu2003@163.com>
2024-03-01 00:23:04 -0800

    add feature: 以图搜图

--- src/plugins/setu/__init__.py
+++ src/plugins/setu/__init__.py
@@ -2,11 +2,12 @@ from typing import Annotated
 from nonebot import get_plugin_config
 from nonebot.plugin import PluginMetadata
 from nonebot import on_command, on_fullmatch
-from nonebot.adapters import Event
+from nonebot.adapters.onebot.v11 import MessageEvent
 from nonebot.params import CommandArg, Fullmatch
 
 from .config import Config
 from .get_setu import get_setu
+from .search_setu import search_setu
 
 __plugin_meta__ = PluginMetadata(
     name="setu",
@@ -25,7 +26,16 @@ async def setu_handle(args = CommandArg()):
     message = get_setu(setu_tags)
     await setu.finish(message = message)
 
-search = on_fullmatch("/setu search")
+search = on_fullmatch("/setu search", priority = 1)
 @search.handle()
-async def search_handle(keyword: Annotated[str, Fullmatch()]):
-    await search.finish(message = str(keyword))
+async def search_handle(event: MessageEvent):
+    source_url = None
+    for seg in event.reply.message:
+        if seg.type == "image":
+            source_url = seg.data.get("url")
+            break
+    if source_url: result = search_setu(source_url)
+    result_str = ""
+    for item in result:
+        result_str += f"{item}\n"
+    await search.finish(result_str)
---
+++ src/plugins/setu/search_setu.py
@@ -0,0 +1,30 @@
+from saucenao_api import SauceNao
+import re
+from typing import List, Dict
+
+from sys import path
+path.append("D:/Bi_Gu-bot/passwords")
+from passwords import get_passwords
+
+def search_setu(setu_url: str, search_num: int = 1) -> List[Dict[str, str]]:
+    saucenao = SauceNao(get_passwords("saucenao_api"))
+    md5_value = extract_MD5(setu_url)
+    target_url = f"https://gchat.qpic.cn/gchatpic_new/0/0-0-{md5_value}/0?term=0"
+    results = saucenao.from_url(target_url)
+    results_dict_list = []
+    for i in range(max(search_num, len(results))):
+        results_dict = {}
+        if results[i].title: results_dict["title"] = results[i].title
+        if results[i].similarity: results_dict["similarity"] = results[i].similarity
+        if results[i].author: results_dict["author"] = results[i].author
+        if results[i].urls: results_dict["urls"] = results[i].urls[0]
+        results_dict_list.append(results_dict)
+    return results_dict_list
+
+def extract_MD5(url: str) -> str:
+    md5_pattern = re.compile(r"[a-fA-F0-9]{32}")
+    md5_values = md5_pattern.findall(url)
+    return md5_values[0]
+    
+
+print(search_setu("https://multimedia.nt.qq.com.cn/gchatpic_new/2306096281/2147563170-2972887728-503CE557F63697DC9D89F5ADD79B99DF/0?term=255&is_origin=1"))

b0ab497901ffe65caa8d51c1ae3870c5482c75bd
Bi_Gu <bigu2003@163.com>
2024-04-07 14:18:55 -0700

    miscellaneous

--- src/plugins/steam/__init__.py
+++ src/plugins/steam/__init__.py
@@ -1,7 +1,8 @@
 from nonebot import get_plugin_config
 from nonebot.plugin import PluginMetadata
-from nonebot import on_command
+from nonebot import on_command, get_bot
 from nonebot.params import CommandArg
+from nonebot_plugin_apscheduler import scheduler
 
 from .config import Config
 from .get_steam_playing import get_steam_playing
@@ -28,5 +29,19 @@ async def steam_handle(args = CommandArg()):
         message = f"{steam_status[0]} 正在玩 {steam_status[1]}"
     await steam.finish(message = message)
 
+steam_status = {"76561198446560670": None, #zjh
+                "76561199115642793": None, #me
+                }
+
+@scheduler.scheduled_job("interval", minutes = 1)
+async def report_steam_status():
+    global steam_status
+    print(steam_status)
+    bot = get_bot("1176129206")
+    for key, value in steam_status.items():
+        username, current_steam_status = get_steam_playing(key)
+        if value != current_steam_status and current_steam_status is not None:
+            steam_status[key] = current_steam_status
+            await bot.send_group_msg(group_id = 157563170, message = f"{username} 正在玩 {current_steam_status}")
 
 

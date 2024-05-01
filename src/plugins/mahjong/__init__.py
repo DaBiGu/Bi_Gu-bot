3b4a3fd6ff4f8890bddbe22ea5146227afb7f4fd
Bi_Gu <bigu2003@163.com>
2024-05-01 13:32:23 -0700

    add mahjong club info (beta version)

--- src/plugins/mahjong/__init__.py
+++ src/plugins/mahjong/__init__.py
@@ -5,6 +5,7 @@ from nonebot.params import CommandArg
 
 from .config import Config
 from .mahjongsoul import get_user_data
+from .mahjongclub import get_mahjongclub_info
 
 __plugin_meta__ = PluginMetadata(
     name="mahjong",
@@ -22,4 +23,13 @@ async def qh_handle(args = CommandArg()):
     username = args.extract_plain_text()
     message = get_user_data(username)
     await qh.finish(message = message)
+
+qz = on_command("qz", aliases={"雀庄"})
+
+@qz.handle()
+async def qz_handle(args = CommandArg()):
+    username = args.extract_plain_text()
+    message = get_mahjongclub_info(username)
+    await qz.finish(message = message)
+
 

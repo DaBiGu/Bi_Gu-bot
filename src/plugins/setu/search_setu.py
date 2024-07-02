30c27afa263acc322c22736189bf68e861d325c5
Bi_Gu <bigu2003@163.com>
2024-07-03 04:20:49 +0800

    update saucenao source url

--- src/plugins/setu/search_setu.py
+++ src/plugins/setu/search_setu.py
@@ -9,16 +9,18 @@ from passwords import get_passwords
 
 def search_setu(setu_url: str, search_num: int = 1) -> List[Dict[str, str]]:
     saucenao = SauceNao(get_passwords("saucenao_api"))
-    md5_value = extract_MD5(setu_url)
-    target_url = f"https://gchat.qpic.cn/gchatpic_new/0/0-0-{md5_value}/0?term=0"
-    results = saucenao.from_url(target_url)
+    #md5_value = extract_MD5(setu_url)
+    #target_url = f"https://gchat.qpic.cn/gchatpic_new/0/0-0-{md5_value}/0?term=0"
+    #results = saucenao.from_url(target_url)
+    results = saucenao.from_url(setu_url)
     results_dict_list = []
     for i in range(max(search_num, len(results))):
         results_dict = {}
         if results[i].title: results_dict["title"] = results[i].title
         if results[i].similarity: results_dict["similarity"] = results[i].similarity
         if results[i].author: results_dict["author"] = results[i].author
-        if results[i].urls: results_dict["urls"] = results[i].urls[0]
+        if results[i].urls: results_dict["url"] = results[i].urls[0]
+        if "source" in results[i].raw["data"]: results_dict["url"] = results[i].raw["data"]["source"]
         results_dict_list.append(results_dict)
     return results_dict_list
 

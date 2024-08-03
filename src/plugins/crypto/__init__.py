8dd72c5bb7c51ed8167a1fe1c466ed6f65b5059a
Bi_Gu <bigu2003@163.com>
2024-08-04 05:03:32 +0800

    add crypto fear&greed index

--- src/plugins/crypto/__init__.py
+++ src/plugins/crypto/__init__.py
@@ -6,6 +6,7 @@ from nonebot.params import CommandArg
 from .config import Config
 from .okx_market_data import get_market_data
 from .okx_market_kline import get_crypto_kline
+from .fear_greed_index import get_market_fear_greed_index
 
 __plugin_meta__ = PluginMetadata(
     name="crypto",
@@ -16,8 +17,8 @@ __plugin_meta__ = PluginMetadata(
 
 config = get_plugin_config(Config)
 
-crpyto = on_command("crypto")
-@crpyto.handle()
+crypto = on_command("crypto")
+@crypto.handle()
 async def crypto_handle(args = CommandArg()):
     if cmd_params := args.extract_plain_text():
         if " " in cmd_params:
@@ -26,13 +27,17 @@ async def crypto_handle(args = CommandArg()):
                 crypto_name = cmd_params[0]
                 time_interval = cmd_params[1]
                 if time_interval not in ["15m", "1h", "4h", "1D", "3D", "1W"]:
-                    await crpyto.finish(message = "时间间隔不合法, 目前支持15m/1h/4h/1D/3D/1W")
+                    await crypto.finish(message = "时间间隔不合法, 目前支持15m/1h/4h/1D/3D/1W")
                 else:
                     message = get_crypto_kline(crypto_name, time_interval)
-                    await crpyto.finish(message = message)
+                    await crypto.finish(message = message)
         else:
-            message = get_crypto_kline(cmd_params)
-            await crpyto.finish(message = message)
+            if cmd_params == "index":
+                message = get_market_fear_greed_index()
+                await crypto.finish(message = message)
+            else:
+                message = get_crypto_kline(cmd_params)
+                await crypto.finish(message = message)
     else:
         message = get_market_data()
-        await crpyto.finish(message = message)
+        await crypto.finish(message = message)

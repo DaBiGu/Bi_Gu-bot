af47a2036817681081ee2aeb17928bbc10e17cda
Bi_Gu <bigu2003@163.com>
2024-08-05 11:00:43 +0800

    miscellaneous

--- src/plugins/miscellaneous/__init__.py
+++ src/plugins/miscellaneous/__init__.py
@@ -50,9 +50,9 @@ skd = on_command("懂哥")
 @skd.handle()
 async def skd_handle_func(args = CommandArg()):
     if args.extract_plain_text() == "工口":
-        await skd.finish(Message([MessageSegment.image("file:///" + os.getcwd() + "/src/data/mstbt/gongkou.png")]))
+        await skd.finish(Message([MessageSegment.image("file:///" + os.getcwd() + "/src/data/miscellaneous/gongkou.png")]))
     elif args.extract_plain_text() == "手冲":
-        await skd.finish(Message([MessageSegment.image("file:///" + os.getcwd() + "/src/data/mstbt/shouchong.png")]))
+        await skd.finish(Message([MessageSegment.image("file:///" + os.getcwd() + "/src/data/miscellaneous/shouchong.png")]))
     else:
         await skd.finish("我好想做上科大的狗啊")
 
@@ -63,7 +63,7 @@ _gk = on_command("工口")
 async def gk_handle_func():
     global gk_time
     gk_time = time.time()
-    await _gk.finish(Message([MessageSegment.image("file:///" + os.getcwd() + "/src/data/mstbt/gongkou.png")]))
+    await _gk.finish(Message([MessageSegment.image("file:///" + os.getcwd() + "/src/data/miscellaneous/gongkou.png")]))
 
 _sc = on_command("手冲", aliases= {"mstbt"})
 @_sc.handle()
@@ -78,7 +78,7 @@ async def sc_handle_func(event: MessageEvent, args = CommandArg()):
                 username = member["nickname"]
                 break
     if args.extract_plain_text() == "counter":
-        with open("./src/data/mstbt/mstbt_counter.txt", "r") as f:
+        with open("./src/data/miscellaneous/mstbt_counter.txt", "r") as f:
             mstbt_counter = float(f.read())
         mstbt_progress = "{:.4f}".format(mstbt_counter / 50)
         mstbt_progress_percent = "{:.2f}".format(float(mstbt_progress) * 100)
@@ -94,22 +94,22 @@ async def sc_handle_func(event: MessageEvent, args = CommandArg()):
     random.seed(time.time())
     global gk_time
     if random.randint(1, 100) == 1 or (gk_time is not None and time.time() - gk_time <= 300):
-        await _sc.send(Message([MessageSegment.image("file:///" + os.getcwd() + "/src/data/mstbt/shouchong.gif")]))
+        await _sc.send(Message([MessageSegment.image("file:///" + os.getcwd() + "/src/data/miscellaneous/shouchong.gif")]))
         await _sc.send("你触发了至臻手冲!")
         await _sc.send(Message(["你获得了手冲奖励：一张随机色图"]) + get_setu())
     else:
-        await _sc.send(Message([MessageSegment.image("file:///" + os.getcwd() + "/src/data/mstbt/shouchong.png")]))
+        await _sc.send(Message([MessageSegment.image("file:///" + os.getcwd() + "/src/data/miscellaneous/shouchong.png")]))
         _mstbt = random.uniform(1, 20)
-        with open("./src/data/mstbt/mstbt_counter.txt", "r") as f:
+        with open("./src/data/miscellaneous/mstbt_counter.txt", "r") as f:
             mstbt_counter = float(f.read())
         mstbt_counter += _mstbt
         if _mstbt == 1:
             await _sc.send("你获得了手冲奖励...吗?")
-            await _sc.send(Message([MessageSegment.image("file:///" + os.getcwd() + "/src/data/mstbt/eat_icecream.png")]))
+            await _sc.send(Message([MessageSegment.image("file:///" + os.getcwd() + "/src/data/miscellaneous/eat_icecream.png")]))
         elif mstbt_counter >= 50:
             await _sc.send(Message(["你获得了手冲奖励：一张随机色图"]) + get_setu())
             mstbt_counter = 0
-        with open("./src/data/mstbt/mstbt_counter.txt", "w") as f:
+        with open("./src/data/miscellaneous/mstbt_counter.txt", "w") as f:
             f.write(str(mstbt_counter))
     await _sc.finish(message_str)
     
@@ -118,13 +118,13 @@ yuyu = on_command("玉玉", aliases= {"yuyu"})
 
 @yuyu.handle()
 async def yuyu_handle_func():
-    await yuyu.finish(Message([MessageSegment.image("file:///" + os.getcwd() + "/src/data/mstbt/yuyu.gif")]))
+    await yuyu.finish(Message([MessageSegment.image("file:///" + os.getcwd() + "/src/data/miscellaneous/yuyu.gif")]))
 
 fufu = on_command("芙芙", aliases= {"fufu"})
 
 @fufu.handle()
 async def fufu_handle_func():
-    await fufu.finish(Message([MessageSegment.image("file:///" + os.getcwd() + "/src/data/mstbt/fufu.gif")]))
+    await fufu.finish(Message([MessageSegment.image("file:///" + os.getcwd() + "/src/data/miscellaneous/fufu.gif")]))
 
 sb = on_fullmatch("阿姨洗铁路")
 

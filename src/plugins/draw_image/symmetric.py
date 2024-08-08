c9c5875932888f41babd055fa585eb68f47ee07f
Bi_Gu <bigu2003@163.com>
2024-08-08 17:31:29 +0800

    [bug fix] let image symmetry right & down behave correctly

--- src/plugins/draw_image/symmetric.py
+++ src/plugins/draw_image/symmetric.py
@@ -7,21 +7,15 @@ def _symmetric(original_image_path: str, direction: str, percent = 50):
     width, height = original_image.size
     _width, _height = int(width * percent / 100), int(height * percent / 100)
     result = Image.new("RGB", (2*_width, height)) if direction in ["left", "right"] else Image.new("RGB", (width, 2*_height))
-    if direction == "left":
+    if direction in ["left", "right"]:
+        original_image = original_image.transpose(Image.FLIP_LEFT_RIGHT) if direction == "right" else original_image
         left = original_image.crop((0, 0, _width, height))
         result.paste(left, (0, 0))
         result.paste(left.transpose(Image.FLIP_LEFT_RIGHT), (_width, 0))
-    elif direction == "right":
-        right = original_image.crop((_width, 0, width, height))
-        result.paste(right.transpose(Image.FLIP_LEFT_RIGHT), (0, 0))
-        result.paste(right, (_width, 0))
-    elif direction == "up":
+    elif direction in ["up", "down"]:
+        original_image = original_image.transpose(Image.FLIP_TOP_BOTTOM) if direction == "down" else original_image
         up = original_image.crop((0, 0, width, _height))
         result.paste(up, (0, 0))
         result.paste(up.transpose(Image.FLIP_TOP_BOTTOM), (0, _height))
-    elif direction == "down":
-        down = original_image.crop((0, _height, width, height))
-        result.paste(down.transpose(Image.FLIP_TOP_BOTTOM), (0, 0))
-        result.paste(down, (0, _height))
     result.save(os.path.join(os.getcwd(), f"src/data/draw_image/symmetric_{direction}.png"))
     return MessageSegment.image("file:///" + os.path.join(os.getcwd(), f"src/data/draw_image/symmetric_{direction}.png"))

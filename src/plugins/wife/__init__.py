dda1a97cb45a87fe08c1e6cbc0f3a31d6c462ae9
Bi_Gu <bigu2003@163.com>
2024-08-19 17:22:39 +0800

    [bug fix]make wife plugin work normally

--- src/plugins/wife/__init__.py
+++ src/plugins/wife/__init__.py
@@ -21,19 +21,22 @@ wife = on_command("wife", aliases={"群老婆"})
 
 @wife.handle()
 async def wife_handle(bot: Bot, event: GroupMessageEvent):
-    group_id = event.group_id
-    user_id = event.user_id
+    group_id = str(event.group_id)
+    user_id = str(event.user_id)
     today = datetime.datetime.now().strftime("%Y-%m-%d")
     with open(os.getcwd() + "/src/data/wife/record.json", "r") as f: record = json.load(f)
     if today not in record: record[today] = {}
     if group_id not in record[today]: record[today][group_id] = {}
-    raw_group_members = await bot.get_group_member_list(group_id = group_id)
-    single_members = []
-    for member in raw_group_members:
-        if member["user_id"] != user_id and member["user_id"] not in record[today][group_id]: single_members.append(member["user_id"])
-    _wife = random.choice(single_members)
-    record[today][group_id][user_id] = _wife
-    record[today][group_id][_wife] = user_id
+    if user_id in record[today][group_id]:
+        _wife = record[today][group_id][user_id]
+    else:
+        raw_group_members = await bot.get_group_member_list(group_id = event.group_id)
+        single_members = []
+        for member in raw_group_members:
+            if str(member["user_id"]) != user_id and str(member["user_id"]) not in record[today][group_id]: single_members.append(str(member["user_id"]))
+        _wife = random.choice(single_members)
+        record[today][group_id][user_id] = _wife
+        record[today][group_id][_wife] = user_id
     with open(os.getcwd() + "/src/data/wife/record.json", "w") as f: json.dump(record, f)
     with open(os.getcwd() + f"/src/data/wife/temp/{user_id}.png", "wb") as f: f.write(requests.get(f"https://q1.qlogo.cn/g?b=qq&nk={user_id}&s=640").content)
     message = Message([MessageSegment.at(user_id), MessageSegment.text("你今天的群老婆是"), MessageSegment.at(_wife),

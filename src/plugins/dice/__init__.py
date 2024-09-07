c12c65a4ccba066b68b205a8959708e4deeeb8d3
Bi_Gu <bigu2003@163.com>
2024-09-07 22:20:30 +0800

    [feature] today's fortune

--- src/plugins/dice/__init__.py
+++ src/plugins/dice/__init__.py
@@ -2,6 +2,7 @@ from nonebot import get_plugin_config
 from nonebot.plugin import PluginMetadata
 from nonebot import on_command
 from nonebot.params import CommandArg
+from nonebot.adapters.onebot.v11 import MessageEvent, Message, MessageSegment
 
 from .config import Config
 import random
@@ -35,3 +36,17 @@ async def rd_handle_func(args=CommandArg()):
                 await rd.finish(roll_dice(int(dice_num), dice_face, dice_add))
             else:
                 await rd.finish(roll_dice(int(dice_num), int(dice_face)))
+
+ys = on_command("ys", aliases={"今日运势"})
+
+@ys.handle()
+async def ys_handle_func(event: MessageEvent):
+    user_id = event.user_id
+    fortune = [random.randint(0, 20) for _ in range(5)]
+    love = [random.randint(0, 20) for _ in range(5)]
+    career = [random.randint(0, 20) for _ in range(5)]
+    message = Message([MessageSegment.at(user_id), MessageSegment.text(f" 今日运势为:\n \
+            财运: 5d20 {str(fortune)} = {sum(fortune)}\n \
+            桃花运: 5d20 {str(love)} = {sum(love)}\n \
+            事业运: 5d20 {str(career)} = {sum(career)}")])
+    await ys.finish(message)

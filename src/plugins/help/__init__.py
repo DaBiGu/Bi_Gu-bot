7c4f3ae378ad86c67b7832c2b929537beccfc0e8
Bi_Gu <bigu2003@163.com>
2024-09-12 19:12:47 +0800

    minor bug fix

--- src/plugins/help/__init__.py
+++ src/plugins/help/__init__.py
@@ -24,16 +24,12 @@ _help = on_command("help", aliases = {"帮助"})
 
 @_help.handle()
 async def help_handle():
-    message = Message()
     for i in range(len(helper_messages)):
-        message += Message([draw_help(helper_messages[i], i+1, len(helper_messages))])
-    await _help.finish(message = message)
+        await _help.send(draw_help(helper_messages[i], i+1, len(helper_messages)))
 
 _update_log = on_fullmatch("/update log", priority = 1)
 
 @_update_log.handle()
 async def update_log_handle():
-    message = Message()
     for i in range(len(update_logs)):
-        message += Message([draw_help(update_logs[i], i+1, len(update_logs))])
-    await _update_log.finish(message = message)
+        await _update_log.send(draw_help(update_logs[i], i+1, len(update_logs)))

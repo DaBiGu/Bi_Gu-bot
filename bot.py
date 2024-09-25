e351542a25c59c85957a7be6ad8c9dd97c637140
Bi_Gu <bigu2003@163.com>
2024-09-25 16:25:53 +0800

    remove bot console ui startup

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import nonebot, os
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
-from nonebot.adapters.console import Adapter as CONSOLEAdapter
 from sys import path
 path.append(os.getcwd() + "/passwords")
 path.append(os.getcwd() + "/src")
@@ -9,8 +8,6 @@ nonebot.init()
 
 driver = nonebot.get_driver()
 driver.register_adapter(ONEBOT_V11Adapter)
-
-driver.register_adapter(CONSOLEAdapter)
 
 nonebot.load_builtin_plugins('echo', 'single_session')
 

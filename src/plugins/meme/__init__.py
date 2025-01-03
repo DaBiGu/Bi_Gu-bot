2e200a25e50738a20cf1d8c239a2e7223eb6a253
Bi_Gu <bigu2003@163.com>
2025-01-04 01:40:41 +0800

    [feature] get httpcat meme

--- src/plugins/meme/__init__.py
+++ src/plugins/meme/__init__.py
@@ -1,9 +1,14 @@
 from nonebot import get_plugin_config
 from nonebot.plugin import PluginMetadata
 from nonebot import on_command
+from nonebot.params import CommandArg
 from nonebot.adapters.onebot.v11 import Message, MessageSegment
 from utils.utils import get_asset_path
 from .config import Config
+
+import requests
+
+from utils.utils import get_output_path
 
 __plugin_meta__ = PluginMetadata(
     name="meme",
@@ -39,5 +44,16 @@ biebie = on_command("憋憋", aliases= {"biebie"})
 async def biebie_handle():
     await biebie.finish(Message([MessageSegment.image("file:///" + get_asset_path("images/biebie.png"))]))
 
+httpcat = on_command("httpcat")
+@httpcat.handle()
+async def httpcat_handle(args = CommandArg()):
+    code = args.extract_plain_text()
+    output_path = get_output_path("httpcat", temp = True)
+    response = requests.get(f"https://http.cat/{code}.jpg")
+    if response.status_code == 200:
+        with open(output_path, "wb") as f: f.write(response.content)
+        await httpcat.finish(Message([MessageSegment.image("file:///" + output_path)]))
+    else: await httpcat.finish("未找到对应状态码的图片")
+
 yuyu.append_handler(yuyu_handle); fufu.append_handler(fufu_handle); die.append_handler(die_handle)
-pupu.append_handler(pupu_handle); biebie.append_handler(biebie_handle)
+pupu.append_handler(pupu_handle); biebie.append_handler(biebie_handle); httpcat.append_handler(httpcat_handle)

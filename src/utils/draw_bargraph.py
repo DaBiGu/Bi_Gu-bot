8fd7b92e30a16bbdbb461e87c7c25efd23481fb5
Bi_Gu <bigu2003@163.com>
2025-12-22 15:57:12 +0800

    fix matplotlib font problem

--- src/utils/draw_bargraph.py
+++ src/utils/draw_bargraph.py
@@ -7,10 +7,14 @@ from utils.utils import get_copyright_str, get_output_path, get_IO_path
 from utils.fonts import get_font_path
 from matplotlib import cm
 from matplotlib.colors import Normalize
+from matplotlib.font_manager import FontProperties
 from .utils import get_datelist
 
 def mix_color(color, white=(1, 1, 1), ratio=0.5):
     return [(1 - ratio) * c + ratio * w for c, w in zip(color, white)]
+
+def get_fontprop(path: str, size: int = None) -> FontProperties:
+    return FontProperties(fname = path, size = size) if size else FontProperties(fname = path)
 
 async def draw_bargraph(data: Dict[str, int], title: str, ylabel: str, nicknames: Dict[int, str], time_range: str = None, kawaii: bool = True) -> MessageSegment:
     support_time_range = time_range is not None
@@ -55,18 +59,18 @@ async def draw_bargraph(data: Dict[str, int], title: str, ylabel: str, nicknames
         x = bar.get_x() + bar.get_width() / 2 - avatar_width / 2
         y = bar.get_height()
         ax.imshow(avatar, extent=(x, x + avatar_width, y, y + avatar_height), aspect='auto')
-        ax.text(bar.get_x() + bar.get_width() / 2, bar.get_height() / 2, f'{bar.get_height()}', ha='center', va='center', font = font_path)
+        ax.text(bar.get_x() + bar.get_width() / 2, bar.get_height() / 2, f'{bar.get_height()}', ha='center', va='center', fontproperties = get_fontprop(font_path))
 
     ax.set_xlim(*xlim)
     ax.set_ylim(*ylim)
-    ax.set_xticklabels(_nicknames, font = font_path, fontsize = 12)
-    ax.set_yticklabels([int(y) for y in ax.get_yticks()], font = font_path, fontsize = 12)
-    fig.text(0.75, -0.1, get_copyright_str(), ha='center', font = font_path, fontsize = 12)
-    plt.ylabel(ylabel, font = font_path)
-    plt.title(title, font = font_path, fontsize = 16, loc='center', x=0.5, y=1.05)
+    ax.set_xticklabels(_nicknames, fontproperties = get_fontprop(font_path, 12))
+    ax.set_yticklabels([int(y) for y in ax.get_yticks()], fontproperties = get_fontprop(font_path, 12))
+    fig.text(0.75, -0.1, get_copyright_str(), ha='center', fontproperties = get_fontprop(font_path, 12))
+    plt.ylabel(ylabel, fontproperties = get_fontprop(font_path, 12))
+    plt.title(title, fontproperties = get_fontprop(font_path, 16), loc='center', x=0.5, y=1.05)
     if support_time_range:
         now = "23:59" if time_range in ["yesterday", "last week", "last month"] else datetime.datetime.now().strftime("%H:%M")
-    fig.text(0.75, 0.9, f"数据范围: {get_datelist(time_range)[0]} 00:00 至{get_datelist(time_range)[-1]} {now}", ha='center', font = font_path, fontsize = 12)
+    fig.text(0.75, 0.9, f"数据范围: {get_datelist(time_range)[0]} 00:00 至{get_datelist(time_range)[-1]} {now}", ha='center', fontproperties = get_fontprop(font_path, 12))
     plt.xticks(rotation=45)
     output_path = get_output_path("bargraph")
     plt.savefig(output_path, dpi = 300, bbox_inches = "tight")
